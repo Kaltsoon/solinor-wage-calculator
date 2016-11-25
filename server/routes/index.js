96b3e94629d6a94762f8df398fa3319439bdca12
Kalle O Ilves <kalleilv@mba-13.cs.helsinki.fi>
2016-11-25 19:35:56 +0200

    cors

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+const cors = require('cors');
+
 
 router.use('/', require('./pages'));
+router.use('/api', cors());
 router.use('/api', require('./api'));
 
 module.exports = router;
