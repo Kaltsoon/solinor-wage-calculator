eca71c5ccdacb61cdc14f3afa236617ad8d7633b
Kalle O Ilves <kalleilv@mba-13.cs.helsinki.fi>
2016-11-25 19:39:40 +0200

    refactoring

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const cors = require('cors');
 
-
 router.use('/', require('./pages'));
 router.use('/api', cors());
 router.use('/api', require('./api'));
